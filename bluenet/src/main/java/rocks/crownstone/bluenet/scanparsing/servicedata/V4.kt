f48078a367c71bc8d3fab6b7f58f5a78a4383086
vliedel <bart@crownstone.rocks>
2018-09-25 17:27:15 +0200

    No decryption for service data V4 and V6.

--- bluenet/src/main/java/rocks/crownstone/bluenet/scanparsing/servicedata/V4.kt
+++ bluenet/src/main/java/rocks/crownstone/bluenet/scanparsing/servicedata/V4.kt
@@ -1,10 +1,8 @@
 package rocks.crownstone.bluenet.scanparsing.servicedata
 
 import rocks.crownstone.bluenet.OperationMode
-import rocks.crownstone.bluenet.encryption.Encryption
 import rocks.crownstone.bluenet.scanparsing.CrownstoneServiceData
 import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 internal object V4 {
 	fun parseHeader(bb: ByteBuffer, servicedata: CrownstoneServiceData): Boolean {
@@ -17,19 +15,6 @@ internal object V4 {
 	}
 
 	fun parse(bb: ByteBuffer, servicedata: CrownstoneServiceData, key: ByteArray?): Boolean {
-		if (key == null) {
-			return parseServiceData(bb, servicedata)
-		}
-		val decryptedBytes = Encryption.decryptEcb(bb, key)
-		if (decryptedBytes == null) {
-			return false
-		}
-		val decryptedBB = ByteBuffer.wrap(decryptedBytes)
-		decryptedBB.order(ByteOrder.LITTLE_ENDIAN)
-		return parseServiceData(decryptedBB, servicedata)
-	}
-
-	private fun parseServiceData(bb: ByteBuffer, servicedata: CrownstoneServiceData): Boolean {
 		servicedata.type = bb.get().toInt()
 		when (servicedata.type) {
 			0 -> return Shared.parseSetupPacket(bb, servicedata, false, false)
--- bluenet/src/main/java/rocks/crownstone/bluenet/scanparsing/servicedata/V6.kt
+++ bluenet/src/main/java/rocks/crownstone/bluenet/scanparsing/servicedata/V6.kt
@@ -2,10 +2,8 @@ package rocks.crownstone.bluenet.scanparsing.servicedata
 
 import rocks.crownstone.bluenet.DeviceType
 import rocks.crownstone.bluenet.OperationMode
-import rocks.crownstone.bluenet.encryption.Encryption
 import rocks.crownstone.bluenet.scanparsing.CrownstoneServiceData
 import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 internal object V6 {
 	fun parseHeader(bb: ByteBuffer, servicedata: CrownstoneServiceData): Boolean {
@@ -18,19 +16,6 @@ internal object V6 {
 	}
 
 	fun parse(bb: ByteBuffer, servicedata: CrownstoneServiceData, key: ByteArray?): Boolean {
-		if (key == null) {
-			return parseServiceData(bb, servicedata)
-		}
-		val decryptedBytes = Encryption.decryptEcb(bb, key)
-		if (decryptedBytes == null) {
-			return false
-		}
-		val decryptedBB = ByteBuffer.wrap(decryptedBytes)
-		decryptedBB.order(ByteOrder.LITTLE_ENDIAN)
-		return parseServiceData(decryptedBB, servicedata)
-	}
-
-	private fun parseServiceData(bb: ByteBuffer, servicedata: CrownstoneServiceData): Boolean {
 		servicedata.type = bb.get().toInt()
 		when (servicedata.type) {
 			0 -> return Shared.parseSetupPacket(bb, servicedata, false, false)
