35ab4faf72d5e5bfdb434b1ffbe39d49fb7463b2
vliedel <bart@crownstone.rocks>
2018-10-31 18:44:52 +0100

    added delays to old setup

--- bluenet/src/main/java/rocks/crownstone/bluenet/services/Setup.kt
+++ bluenet/src/main/java/rocks/crownstone/bluenet/services/Setup.kt
@@ -74,36 +74,63 @@ class Setup(evtBus: EventBus, connection: ExtConnection) {
 		// TODO: increase TX?
 		return config.setCrownstoneId(id)
 				.then {
+					connection.wait(500)
+				}.unwrap()
+				.then {
 					sendProgress(OldSetupStep.ID_WRITTEN)
 					config.setAdminKey(adminKey)
+				}.unwrap()
+				.then {
+					connection.wait(500)
 				}.unwrap()
 				.then {
 					sendProgress(OldSetupStep.ADMIN_KEY_WRITTEN)
 					config.setMemberKey(memberKey)
 				}.unwrap()
 				.then {
+					connection.wait(500)
+				}.unwrap()
+				.then {
 					sendProgress(OldSetupStep.MEMBER_KEY_WRITTEN)
 					config.setGuestKey(guestKey)
+				}.unwrap()
+				.then {
+					connection.wait(500)
 				}.unwrap()
 				.then {
 					sendProgress(OldSetupStep.GUEST_KEY_WRITTEN)
 					config.setMeshAccessAddress(meshAccessAddress)
 				}.unwrap()
 				.then {
+					connection.wait(500)
+				}.unwrap()
+				.then {
 					sendProgress(OldSetupStep.MESH_ADDRESS_WRITTEN)
 					config.setIbeaconUuid(ibeaconData.uuid)
+				}.unwrap()
+				.then {
+					connection.wait(500)
 				}.unwrap()
 				.then {
 					sendProgress(OldSetupStep.IBEACON_UUID_WRITTEN)
 					config.setIbeaconMajor(ibeaconData.major)
 				}.unwrap()
 				.then {
+					connection.wait(500)
+				}.unwrap()
+				.then {
 					sendProgress(OldSetupStep.IBEACON_MAJOR_WRITTEN)
 					config.setIbeaconMinor(ibeaconData.minor)
 				}.unwrap()
 				.then {
+					connection.wait(500)
+				}.unwrap()
+				.then {
 					sendProgress(OldSetupStep.IBEACON_MINOR_WRITTEN)
 					control.validateSetup()
+				}.unwrap()
+				.then {
+					connection.wait(500)
 				}.unwrap()
 				.then {
 					sendProgress(OldSetupStep.FINALIZE_WRITTEN)
