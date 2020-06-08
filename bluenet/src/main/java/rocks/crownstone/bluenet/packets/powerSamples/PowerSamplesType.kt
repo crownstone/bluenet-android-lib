452d46ff94a6c284fc76d249ac8a53f37b8f8cfb
vliedel <bart@crownstone.rocks>
2020-06-08 15:54:27 +0200

    Added new power samples types.

--- bluenet/src/main/java/rocks/crownstone/bluenet/packets/powerSamples/PowerSamplesType.kt
+++ bluenet/src/main/java/rocks/crownstone/bluenet/packets/powerSamples/PowerSamplesType.kt
@@ -12,6 +12,8 @@ import rocks.crownstone.bluenet.structs.Uint8
 enum class PowerSamplesType(val num: Uint8) {
 	SWITCHCRAFT(0U),
 	SWITCHCRAFT_NON_TRIGGERED(1U),
+	NOW(3U),
+	NOW_UNFILTERED(4U),
 	UNKNOWN(255U);
 	companion object {
 		private val map = values().associateBy(PowerSamplesType::num)
@@ -25,6 +27,8 @@ fun PowerSamplesIndices(type: PowerSamplesType): MutableList<Uint8> {
 	return when (type) {
 		PowerSamplesType.SWITCHCRAFT -> arrayListOf(0U, 1U, 2U)
 		PowerSamplesType.SWITCHCRAFT_NON_TRIGGERED -> arrayListOf(0U, 1U, 2U)
+		PowerSamplesType.NOW -> arrayListOf(0U, 1U)
+		PowerSamplesType.NOW_UNFILTERED -> arrayListOf(0U, 1U)
 		PowerSamplesType.UNKNOWN -> arrayListOf()
 	}
 }
